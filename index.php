9f268d83c69b9152ba97ea4031e370f8141df16d
Ingrid Ribeiro <32398486+ervadaninha@users.noreply.github.com>
2018-03-09 15:15:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$donoel = "3";
+$ingrid = "3";
+
+if($donoel > $ingrid) {
+    echo "Parabéns, você é ruim!";
+} elseif ($donoel == $ingrid) {
+    echo "Parabéns, você é fantástico!";
+}
+
